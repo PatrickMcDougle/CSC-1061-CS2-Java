71958632959704b857a6bd13b9c5c3cb3e737786
Patrick McDougle <Patrick.McDougle@gmail.com>
2023-03-16 22:48:31 -0600

    Mandelbrot Code
     - Mandelbrot code that shows of using complex numbers.
     - initial code.
     - Format changes.

---
+++ src/edu/csc1061/mandelbrot/MApp.java
@@ -0,0 +1,28 @@
+package edu.csc1061.mandelbrot;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MApp extends Application {
+
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MApp.fxml"));
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Visualize the Mandelbrot set"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    /**
+     * The main method is only needed for the IDE with limited JavaFX support. Not
+     * needed for running from the command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/edu/csc1061/mandelbrot/Mandelbrot.java
@@ -0,0 +1,27 @@
+package edu.csc1061.mandelbrot;
+
+public class Mandelbrot {
+
+    // no need to instantiate this class.
+    private Mandelbrot() {
+    }
+
+    /**
+     * Does stuff.
+     *
+     * @param n0  The starting complex number
+     * @param max the maximum number of iterations
+     * @return number of iterations to check if c = a + ib is in Mandelbrot set
+     */
+    public static int numberOfIterations(Complex n0, int max) {
+        Complex n = n0;
+
+        for (int i = 0; i < max; ++i) {
+            if (n.abs() > 2.0) {
+                return i;
+            }
+            n = n.times(n).plus(n0);
+        }
+        return max;
+    }
+}
